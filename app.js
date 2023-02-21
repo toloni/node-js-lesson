e0b15ad1a5f1b3eb8763b330af8fbfb52b6e3570
Tiago Toloni <tiago.toloni@gmail.com>
2023-02-20 21:51:09 -0300

    starting middleware

--- app.js
+++ app.js
@@ -3,13 +3,26 @@ const express = require('express');
 
 const app = express();
 app.use(express.json());
+
+app.use((req, res, next) => {
+  console.log('Hello from the middleware!');
+  next();
+});
+
+app.use((req, res, next) => {
+  req.requestTime = new Date().toISOString();
+  next();
+});
+
 const tours = JSON.parse(
   fs.readFileSync(`${__dirname}/dev-data/data/tours-simple.json`)
 );
 
 const getAllTours = (req, res) => {
+  console.log(req.requestTime);
   res.status(200).json({
     status: 'success',
+    requestTime: req.requestTime,
     results: tours.length,
     data: { tours },
   });
