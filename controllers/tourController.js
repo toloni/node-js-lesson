dc6571aa20c0f3467e95b6562c9385b9cfb127f6
Tiago Toloni <tiago.toloni@gmail.com>
2023-02-21 20:18:24 -0300

    api filtering querystring

--- controllers/tourController.js
+++ controllers/tourController.js
@@ -2,11 +2,62 @@ const Tour = require('./../models/tourModel');
 
 exports.getAllTours = async (req, res) => {
   try {
-    const tours = await Tour.find();
+    // console.log(req.query);
 
+    // BUILD QUERY
+    // 1) Filtering
+    const queryObj = { ...req.query };
+    const excludeFields = ['page', 'sort', 'fields'];
+    excludeFields.forEach((el) => delete queryObj[el]);
+
+    //2) Advanced Filtering
+    //http://localhost:8000/api/v1/tours?duration[$lt]=5
+    // gte|gt|lte|lt
+
+    let query = Tour.find(queryObj);
+
+    // SORT
+    //http://localhost:8000/api/v1/tours?sort=price  ?sort=-price
+    if (req.query.sort) {
+      const sortBy = req.query.sort.split(',').join(' ');
+      query = query.sort(sortBy);
+    } else {
+      query = query.sort('-createdAt');
+    }
+
+    // FIELDS "select atribute, atribute"
+    if (req.query.fields) {
+      const fields = req.query.fields.split(',').join(' ');
+      //console.log(fields);
+      query = query.select(fields);
+    } else {
+      query = query.select('-__v');
+    }
+
+    // PAGINATION
+    const page = req.query.page * 1 || 1;
+    const limit = req.query.limit * 1 || 100;
+    const skip = (page - 1) * limit;
+    // ?page=2&limit=10  // skipt - pular 10 itens
+    query = query.skip(skip).limit(limit);
+
+    if (req.query.page) {
+      const numTours = await Tour.countDocuments();
+      if (skip >= numTours) throw new Error('This page is not exist');
+    }
+
+    // EXECUTE
+    const tours = await query;
+
+    // const tours = await Tour.find()
+    //   .where('duration')
+    //   .equals(5)
+    //   .where('diffculty')
+    //   .equals('easy');
     res.status(200).json({
       status: 'success',
       results: tours.length,
+      page: page,
       data: {
         tours,
       },
@@ -14,7 +65,7 @@ exports.getAllTours = async (req, res) => {
   } catch (err) {
     res.status(404).json({
       satus: 'fail',
-      message: err,
+      message: err.message,
     });
   }
 };
