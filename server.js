2dfacadf3bb00e24d60308d1c20626586a55cdb7
Tiago Toloni <tiago.toloni@gmail.com>
2023-02-21 16:38:16 -0300

    crud mongo database

--- server.js
+++ server.js
@@ -14,34 +14,6 @@ mongoose
   .connect(process.env.DATABASE_LOCAL)
   .then(() => console.log('DB connection successful!'));
 
-const tourSchema = new mongoose.Schema({
-  name: {
-    type: String,
-    required: [true, 'A tour must have a name'],
-    unique: true,
-  },
-  rating: { type: Number, default: 4.5 },
-  price: { type: Number, required: [true, 'A tour must have a price'] },
-});
-
-const Tour = mongoose.model('Tour', tourSchema);
-
-const testTour = new Tour({
-  name: 'The Forest Hiker',
-  rating: 4.7,
-  price: 497,
-});
-
-testTour
-  .save()
-  .then((doc) => {
-    console.log(doc);
-    mongoose.connection.close();
-  })
-  .catch((err) => {
-    console.log(err);
-  });
-
 const port = process.env.PORT || 3000;
 app.listen(port, () => {
   console.log(`listening on port ${port} ...`);
