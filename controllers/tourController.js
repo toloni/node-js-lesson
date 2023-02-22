6a2727ebf2d2a28330c213794d46a54d8c2acd74
Tiago Toloni <tiago.toloni@gmail.com>
2023-02-21 21:14:28 -0300

    aliasing and refactoring apifeatures

--- controllers/tourController.js
+++ controllers/tourController.js
@@ -1,63 +1,25 @@
 const Tour = require('./../models/tourModel');
+const APIFeatures = require('./../utils/apiFeatures');
+
+exports.aliasTopTours = (req, res, next) => {
+  req.query.limit = 5;
+  req.query.sort = '-ratingsAverage,price';
+  req.query.fields = 'name.price,ratingsAverage,summary,difficulty';
+  next();
+};
 
 exports.getAllTours = async (req, res) => {
   try {
-    // console.log(req.query);
+    const features = new APIFeatures(Tour.find(), req.query)
+      .filter()
+      .sort()
+      .limitFields()
+      .paginate();
+    const tours = await features.query;
 
-    // BUILD QUERY
-    // 1) Filtering
-    const queryObj = { ...req.query };
-    const excludeFields = ['page', 'sort', 'fields'];
-    excludeFields.forEach((el) => delete queryObj[el]);
-
-    //2) Advanced Filtering
-    //http://localhost:8000/api/v1/tours?duration[$lt]=5
-    // gte|gt|lte|lt
-
-    let query = Tour.find(queryObj);
-
-    // SORT
-    //http://localhost:8000/api/v1/tours?sort=price  ?sort=-price
-    if (req.query.sort) {
-      const sortBy = req.query.sort.split(',').join(' ');
-      query = query.sort(sortBy);
-    } else {
-      query = query.sort('-createdAt');
-    }
-
-    // FIELDS "select atribute, atribute"
-    if (req.query.fields) {
-      const fields = req.query.fields.split(',').join(' ');
-      //console.log(fields);
-      query = query.select(fields);
-    } else {
-      query = query.select('-__v');
-    }
-
-    // PAGINATION
-    const page = req.query.page * 1 || 1;
-    const limit = req.query.limit * 1 || 100;
-    const skip = (page - 1) * limit;
-    // ?page=2&limit=10  // skipt - pular 10 itens
-    query = query.skip(skip).limit(limit);
-
-    if (req.query.page) {
-      const numTours = await Tour.countDocuments();
-      if (skip >= numTours) throw new Error('This page is not exist');
-    }
-
-    // EXECUTE
-    const tours = await query;
-
-    // const tours = await Tour.find()
-    //   .where('duration')
-    //   .equals(5)
-    //   .where('diffculty')
-    //   .equals('easy');
     res.status(200).json({
       status: 'success',
       results: tours.length,
-      page: page,
       data: {
         tours,
       },
